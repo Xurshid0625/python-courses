c95e70f0e3a5c9ecf8d015d85c39c0d73571f464
Xurshid Abdumannonov <abdumannonovxurshid0625@gmail.com>
2026-04-18 08:15:26 +0500

    ready product info

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from main.views import index
+from main.views import index, shop, product
 
 urlpatterns = [
-  path('', index, name='index')
+  path('', index, name='index'),
+  path('shop/', shop, name='shop'),
+  path('product/<int:id>/', product, name='product'),
 ]
--- main/views.py
+++ main/views.py
@@ -2,8 +2,6 @@ from django.shortcuts import render
 from main.models import Categories, Product
 
 
-
-
 def index(request):
     
     category = Categories.objects.all()
@@ -14,3 +12,24 @@ def index(request):
         'product': product,
     }
     return render(request, 'main/index.html', ctx)
+
+
+def shop(request):
+    
+    product = Product.objects.all()
+    
+    ctx = {
+        'product': product,
+    }
+    
+    return render(request,'main/product.html',ctx)
+
+def product(request, id):
+    
+    product_info = Product.objects.filter(categories_id=id)
+    
+    ctx = {
+        'product_info': product_info,
+    }
+    
+    return render(request, 'main/product_info.html',ctx)
